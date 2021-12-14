59fb8d7e92592692336f3b1401c17334636ad5b0
aayushshetty12 <aayush.shetty12@gmail.com>
2021-12-14 18:59:36 +0530

    added storage

--- src/App.js
+++ src/App.js
@@ -1,75 +1,87 @@
-import NoteAddIcon from '@mui/icons-material/NoteAdd';
-import './App.css';
-import CreateNote from './createNote.js'
-import Note from './Note.js';
-import {useState} from 'react';
+import NoteAddIcon from "@mui/icons-material/NoteAdd";
+import "./App.css";
+import CreateNote from "./createNote.js";
+import Note from "./Note.js";
+import { useEffect, useState } from "react";
 
 function App() {
-
-  const [change,setChange]=useState({
-    title:'',
-    content:'',
-    date:''
+  const [change, setChange] = useState({
+    title: "",
+    content: "",
+    date: "",
   });
-  const [notes,setNotes]=useState([])
-  const [show,setShow]=useState(false);
+  const [notes, setNotes] = useState(
+    localStorage.getItem("notes")
+      ? JSON.parse(localStorage.getItem("notes"))
+      : []
+  );
+  const [show, setShow] = useState(false);
 
-  const input=e=>{
-    let name=e.target.id;
-    console.log(typeof(name));
-    setChange({...change,[name]:e.target.value});
-    console.log(change);
-  }
+  const input = (e) => {
+    let name = e.target.id;
+    //console.log(typeof name);
+    setChange({ ...change, [name]: e.target.value });
+    //console.log(change);
+  };
 
-  const submit=(e)=>{
-    let d= new Date();
+  const submit = (e) => {
+    let d = new Date();
     d = d.toLocaleString("en-GB", {
       day: "numeric",
       month: "short",
       year: "numeric",
       hour: "numeric",
-      minute: "2-digit"
+      minute: "2-digit",
     });
-    change.date=d;
-    setNotes([...notes,change])
+    change.date = d;
+    setNotes([change, ...notes]);
     //console.log(notes)
     e.preventDefault();
     setChange({
-      title:'',
-      content:'',
-      date:''
+      title: "",
+      content: "",
+      date: "",
     });
     setShow(false);
-  }
+  };
 
-  const deleteNote=(x)=>{
-      setNotes(notes.filter((note,index)=>index!==x))
-  }
+  const deleteNote = (x) => {
+    setNotes(notes.filter((note, index) => index !== x));
+  };
+
+  useEffect(() => localStorage.setItem("notes", JSON.stringify(notes)), [
+    notes,
+  ]);
 
   return (
     <div className="App">
       <header>
-        <h1><NoteAddIcon/>Note Keeper</h1>
+        <h1>
+          <NoteAddIcon />
+          Note Keeper
+        </h1>
       </header>
-      <div className='input'>
+      <div className="input">
         <CreateNote
           show={show}
           change={change}
           input={input}
           submit={submit}
-          set={()=>setShow(true)}
+          set={() => setShow(true)}
         />
       </div>
-      <div className='main'>
-        {notes.slice(0).reverse().map((note,x)=>
-          <Note
-          title={note.title}
-          content={note.content}
-          index={x}
-          deleteNote={deleteNote}
-          date={note.date}
-          />
-        )}
+      <div className="main">
+        {notes.map((note, x) => {
+          return (
+            <Note
+              title={note.title}
+              content={note.content}
+              index={x}
+              deleteNote={deleteNote}
+              date={note.date}
+            />
+          );
+        })}
       </div>
     </div>
   );
--- src/Note.js
+++ src/Note.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import DeleteIcon from '@mui/icons-material/Delete';
+import React from "react";
+import DeleteIcon from "@mui/icons-material/Delete";
 
-function Note(props){
+function Note(props) {
   return (
-    <div className='output'>
-      <h1>{props.title}</h1>
-      <p>{props.content}</p>
-      <div className='lower'>
+    <div className="output">
+      <div className="upper">
+        <h1>{props.title}</h1>
+        <p>{props.content}</p>
+      </div>
+      <div className="lower">
         <p>{props.date}</p>
-        <button onClick={()=>{props.deleteNote(props.index)}}><DeleteIcon fontSize="large"/></button>
+        <button
+          onClick={() => {
+            props.deleteNote(props.index);
+          }}
+        >
+          <DeleteIcon fontSize="large" />
+        </button>
       </div>
     </div>
   );
